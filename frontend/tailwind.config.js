30451023e36f731fb644b7a25ed31b32046d6e1a
Alexandre Moeri <alex.moeri@gmail.com>
2023-09-16 21:34:15 +0200

    font cleanup

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -9,7 +9,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Kanit', ...defaultTheme.fontFamily.sans]
+        sans: ['Kanit', 'sans-serif']
       }
     },
   },
