40e96feed19034aae5f95517197b1cfeb4b03d87
Fela Calana <felacalana@example.com>
2023-09-16 17:55:06 +0200

    Correct path of english words

--- backend/semantiq/validate.py
+++ backend/semantiq/validate.py
@@ -1,6 +1,6 @@
 import os
 
-WORDS_FILE = os.path.join(os.path.dirname(__file__), '../../assets/english_words.csv')
+WORDS_FILE = os.path.join(os.path.dirname(__file__), '../assets/english_words.csv')
 
 
 def validate_user_input(input_str: str) -> bool:
