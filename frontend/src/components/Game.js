619d3d9904e5ae7df9a996e5567b2d5480d2e967
Fela Calana <felacalana@example.com>
2023-09-17 02:42:15 +0200

    Limit max width of imput field

--- frontend/src/components/Game.js
+++ frontend/src/components/Game.js
@@ -21,7 +21,7 @@ function Game({ puzzle, submit, loadingPuzzle, loadingResult, input, change }) {
         <div className="text-center text-slate-400 text-lg">stay close to</div>
         {wordGroup(match)}
       </section>
-      <section className="text-center my-5 text-slate-800 mt-0 mb-0">
+      <section className="text-center my-5 text-slate-800 mt-0 mb-0 max-w-md mx-auto">
         <form onSubmit={handleSubmit} className="place-items-center text-center">
           <input onChange={change} value={input} type="text" autoFocus placeholder="find a word..." className="text-2xl w-2/3 text-center rounded py-1 mr-1 border-2 border-slate-400 focus-visible:border-slate-400 outline-none focus:outline-none focus-visible:outline-none " />
           <button type="submit" disabled={input === "" || loadingResult} className={`bg-slate-400 hover:bg-slate-600 text-slate-100 border-2 border-slate-400 hover:border-slate-600 py-1 px-4 rounded inline-flex items-center relative top-2 ${input === "" || loadingResult ? 'bg-slate-300 hover:bg-slate-300 border-slate-300 hover:border-slate-300' : null}`}>
